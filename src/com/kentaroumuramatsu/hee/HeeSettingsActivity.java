b2e02c34ce992e94e1de900579c74bb212623021
ken <ken@zynga.co.jp>
2011-09-18 15:40:06 +0900

    文言追加

--- src/com/kentaroumuramatsu/hee/HeeSettingsActivity.java
+++ src/com/kentaroumuramatsu/hee/HeeSettingsActivity.java
@@ -3,7 +3,6 @@ package com.kentaroumuramatsu.hee;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.View.OnClickListener;
 
 
